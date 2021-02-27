1e5d818ef17538f745199c42b2cc888e3430cbb1
flaaaps <julianwening4@gmail.com>
2021-02-27 22:15:53 +0100

    :rocket: Create query

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-const { ApolloServer } = require('apollo-server');
+import { ApolloServer } from 'apollo-server';
 import { readFileSync } from 'fs';
 
 const typeDefs = readFileSync('./src/typeDefs/schema.gql').toString('utf-8');
 
 const resolvers = {
-}
+    Query: {},
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
