db438b102fde42f546556858cce41c062ebfe3e8
flaaaps <julianwening4@gmail.com>
2021-03-01 17:29:04 +0100

    :sparkles: Resolve fixtures and live fixtures

--- src/resolvers/fixtures.ts
+++ src/resolvers/fixtures.ts
@@ -1,2 +1,47 @@
-import mongoose from 'mongoose';
-import FixtureModel from '../models/FixtureModel.js';
+import { Fixture } from '../models/FixtureModel.js';
+
+import { getFixtures, getFixtureById } from '../database.js';
+
+export async function resolveFixtures(_: any, args: { live: boolean }) {
+    const fixtures: Fixture[] = await getFixtures(args.live);
+    return fixtures.map((fixture) => {
+        return {
+            id: fixture.fixture.id,
+            timestamp: fixture.fixture.timestamp,
+            referee: fixture.fixture.referee,
+            goals: fixture.goals,
+            league: fixture.league,
+            teams: fixture.teams,
+            status: fixture.fixture.status,
+            events: fixture.events,
+        };
+    });
+}
+
+export async function resolveFixtureById(_: any, args: { id: string; live: boolean }) {
+    const id = parseInt(args.id);
+    const live = args.live;
+    console.log(id);
+    const fixture: Fixture | null = await getFixtureById(id, live);
+    console.log('Fixture found!');
+    if (fixture) {
+        return parseFixtures([fixture])[0];
+    } else {
+        return null;
+    }
+}
+
+function parseFixtures(fixtures: Fixture[]) {
+    return fixtures.map((fixture) => {
+        return {
+            id: fixture.fixture.id,
+            timestamp: fixture.fixture.timestamp,
+            referee: fixture.fixture.referee,
+            goals: fixture.goals,
+            league: fixture.league,
+            teams: fixture.teams,
+            status: fixture.fixture.status,
+            events: fixture.events,
+        };
+    });
+}
