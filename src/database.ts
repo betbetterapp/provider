aa851ac78a062faa30a7938831ee01d548d3005d
flaaaps <julianwening4@gmail.com>
2021-03-01 17:26:39 +0100

    :sparkles: Create utility functions to fetch from database

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,33 @@
 import mongoose from 'mongoose';
-import FixtureModel from './models/FixtureModel.js';
+import { LiveFixtureModel, FixtureModel } from './models/FixtureModel.js';
 import env from 'dotenv';
 env.config();
 
 export function connect() {
-    return mongoose.connect(process.env.MONGO_URI!!, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+    mongoose
+        .connect(process.env.MONGO_URI!!, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+        .then(() => {
+            console.log('> Successfully connected to db');
+        })
+        .catch(() => {
+            throw new Error('Connection to database unsuccessful');
+        });
 }
+
+async function getFixtures(live: boolean = false) {
+    console.log('Fetching fixtures...');
+    if (!live) {
+        const res = await FixtureModel.find().lean();
+        return res;
+    } else {
+        const res = await LiveFixtureModel.find().lean();
+        return res;
+    }
+}
+
+async function getFixtureById(fixtureId: number, live: boolean) {
+    if (!live) return FixtureModel.findOne({ 'fixture.id': fixtureId });
+    else return LiveFixtureModel.findOne({ 'fixture.id': fixtureId });
+}
+
+export { getFixtures, getFixtureById };
