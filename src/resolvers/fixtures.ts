c929e8cf3dbaaf7b8bd1d74a63ccb6a6014dfbb6
flaaaps <julianwening4@gmail.com>
2021-03-01 22:00:15 +0100

    :art: Remove unnecessary return keyword

--- src/resolvers/fixtures.ts
+++ src/resolvers/fixtures.ts
@@ -4,18 +4,16 @@ import { getFixtures, getFixtureById } from '../database.js';
 
 export async function resolveFixtures(_: any, args: { live: boolean }) {
     const fixtures: Fixture[] = await getFixtures(args.live);
-    return fixtures.map((fixture) => {
-        return {
-            id: fixture.fixture.id,
-            timestamp: fixture.fixture.timestamp,
-            referee: fixture.fixture.referee,
-            goals: fixture.goals,
-            league: fixture.league,
-            teams: fixture.teams,
-            status: fixture.fixture.status,
-            events: fixture.events,
-        };
-    });
+    return fixtures.map((fixture) => ({
+        id: fixture.fixture.id,
+        timestamp: fixture.fixture.timestamp,
+        referee: fixture.fixture.referee,
+        goals: fixture.goals,
+        league: fixture.league,
+        teams: fixture.teams,
+        status: fixture.fixture.status,
+        events: fixture.events,
+    }));
 }
 
 export async function resolveFixtureById(_: any, args: { id: string; live: boolean }) {
