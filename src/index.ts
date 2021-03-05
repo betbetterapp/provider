04c65b0d0423c29407f788e175af0916ffb50737
flaaaps <julianwening4@gmail.com>
2021-03-05 22:43:23 +0100

    :sparkles: Create fixture update subscription & watch fixtures

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, PubSub } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import { readFileSync } from 'fs';
 import http from 'http';
@@ -10,10 +10,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { resolveFixtures, resolveFixtureById } from './resolvers/fixtures.js';
+import mongoose from 'mongoose';
 
 const typeDefs = readFileSync('./src/typeDefs/schema.gql').toString('utf-8');
 
+const FIXTURE_UPDATE = 'FIXTURE_UPDATE';
+
 const resolvers = {
+    Subscription: {
+        fixtureUpdate: {
+            subscribe: (_: any, __: any, { pubsub }: { pubsub: PubSub }) => pubsub.asyncIterator(FIXTURE_UPDATE),
+        },
+    },
     Query: {
         fixtures: resolveFixtures,
         fixtureById: resolveFixtureById,
@@ -21,7 +29,8 @@ const resolvers = {
 };
 
 const app = express();
-const server = new ApolloServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new ApolloServer({ typeDefs, resolvers, context: ({ req, res }) => ({ req, res, pubsub }) });
 const httpServer = http.createServer(app);
 
 app.get('/', (req, res) => {
@@ -31,7 +40,15 @@ app.get('/', (req, res) => {
 app.use(bodyParser.json());
 
 server.applyMiddleware({ app });
-db.connect();
+db.connect().then(() => {
+    const client = mongoose.connection.getClient();
+    const collection = client.db('betbetterDB').collection('fixtures');
+    const changeStream = collection.watch();
+    changeStream.on('change', (next) => {
+        console.log('Next:', next);
+    });
+});
+
 httpServer.listen(process.env.PORT || 4000, () => {
     console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 });
